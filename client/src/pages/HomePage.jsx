09ba1d15b88ea168c2dca95fd4ec9a3e6fa3fc5e
007Aryan007 <123658849+007Aryan007@users.noreply.github.com>
2024-10-30 06:27:13 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
         navigate('/signup');
     };
 
-    return (
+    return (            
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
             <p className="text-lg mb-8">This is the home page of our application.</p>
@@ -28,6 +28,12 @@ const HomePage = () => {
                 >
                     Signup
                 </button>
+                <button 
+                onClick={() => navigate('/books')} 
+                className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700 transition duration-300"
+            >
+                Books
+            </button>
             </div>
         </div>
     );
